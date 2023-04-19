3c887f65dc7cd8170c95f6013e2526915ec86c50
christian-iuil <okoriec01@gmail.com>
2023-04-19 10:04:07 +0100

    cal design is almost done

---
+++ lib/style/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final kBgColor=Colors.grey.withOpacity(0.5);
+const kTextColor=Colors.green;
+const kTextColorII=Colors.red;
--- lib/view/calculator_class.dart
+++ lib/view/calculator_class.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../style/styles.dart';
+
 class Cal extends StatefulWidget {
   const Cal({Key? key}) : super(key: key);
 
@@ -10,6 +12,104 @@ class Cal extends StatefulWidget {
 class _CalState extends State<Cal> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    final mediaquery = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              child: Column(
+                children: [
+                  Text(
+                    '0',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                  Text(
+                    '0',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              child: Divider(
+                thickness: 3,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      button('7',kBgColor,kTextColor),
+                      button('8',kBgColor,kTextColor),
+                      button('9',kBgColor,kTextColor),
+                      button('%',kBgColor,kTextColorII),
+                    ],
+                  ),
+                  sizeHeight(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      button('7',kBgColor,kTextColor),
+                      button('8',kBgColor,kTextColor),
+                      button('9',kBgColor,kTextColor),
+                      button('%',kBgColor,kTextColorII),
+                    ],
+                  ),
+                  sizeHeight(),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      button('7',kBgColor,kTextColor),
+                      button('8',kBgColor,kTextColor),
+                      button('9',kBgColor,kTextColor),
+                      button('%',kBgColor,kTextColorII),
+                    ],
+                  ),
+                  sizeHeight(),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      button('7',kBgColor,kTextColor),
+                      button('8',kBgColor,kTextColor),
+                      button('9',kBgColor,kTextColor),
+                      button('%',kBgColor,kTextColorII),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox sizeHeight() => SizedBox(height: 20,);
+
+  TextButton button( String buttonText,Color bgColor, Color textColor) {
+    return TextButton(
+                      style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all(const Size(80, 80)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(100))),
+                        backgroundColor:
+                            MaterialStateColor.resolveWith((states) => bgColor),
+                      ),
+                      onPressed: ()=>print(buttonText),
+                      child: Text(
+                       buttonText,
+                        style:  TextStyle(fontSize: 30, fontWeight: FontWeight.w600,color: textColor),
+                      ),
+                    );
   }
 }
